9a59bb49ff4f6b16dd624bf28e133619a43e5460
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-02-25 23:54:45 +0100

    Add basic spring boot app multi-modulee project (Closes #1)

---
+++ spring-boot-app-template/src/test/kotlin/com/yonatankarp/springbootapptemplate/SpringBootAppTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yonatankarp.springbootapptemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootAppTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
